f661efb6a1918782efa72816805522e6502b33f3
James Leong <james.leong94@gmail.com>
2020-09-07 09:55:35 -0700

    fix 195

--- daily_coding_problem_195.py
+++ daily_coding_problem_195.py
@@ -15,19 +15,19 @@ And i1 = 1, j1 = 1, i2 = 3, j2 = 3, return 15 as there are 15 numbers in the mat
 
 import unittest
 
-def matrix_range(matrix, i1, i2, j1, j2):
+def matrix_range(matrix, i1, j1, i2, j2):
     result = 0
     height = len(matrix)
     width = len(matrix[0])
     if i1 < 0 or i1 >= height or i2 < 0 or i2 >= width \
     or j1 < 0 or j1 >= height or j2 < 0 or j2 >= width:
         return result
-
-    # calc first point
-    less_than = i1 * width + i2
-    result += less_than
-    more_than =  (height - j1 - 1) * width + (width - 1 - j2)
-    result += more_than
+    sm = matrix[i1][j1]
+    lg = matrix[i2][j2]
+    for row in matrix:
+        for elem in row:
+            if elem < sm or elem > lg:
+                result += 1
     return result
 
 class DailyCodingProblemTest(unittest.TestCase):
@@ -40,10 +40,10 @@ class DailyCodingProblemTest(unittest.TestCase):
         [20, 25, 30, 35, 40, 45]]
 
         i1 = 1
-        i2 = 1
-        j1 = 3
+        j1 = 1
+        i2 = 3
         j2 = 3
-        self.assertEqual(matrix_range(test, i1, i2, j1, j2), 15)
+        self.assertEqual(matrix_range(test, i1, j1, i2, j2), 14)
 
     def test_case_2(self):
         test = [[1, 3, 7, 10, 15, 20],
@@ -53,10 +53,10 @@ class DailyCodingProblemTest(unittest.TestCase):
         [20, 25, 30, 35, 40, 45]]
 
         i1 = 1
-        i2 = 3
-        j1 = 4
+        j1 = 3
+        i2 = 4
         j2 = 4
-        self.assertEqual(matrix_range(test, i1, i2, j1, j2), 10)
+        self.assertEqual(matrix_range(test, i1, j1, j2, j2), 14)
 
 
 
